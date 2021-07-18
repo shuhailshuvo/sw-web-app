d6ed03e77c05347d5c2f911b0214fe8396b09b2d
unknown <mahmudshamim355@gmail.com>
2021-07-18 18:30:07 +0600

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,5 @@
-import React from "react";
-import { Provider } from "react-redux";
-import { Router } from "react-router-dom";
-import { PersistGate } from "redux-persist/es/integration/react";
-import routes from "./routes";
-import { store, persistor } from "./Store";
-
-import { createBrowserHistory } from "history";
-const history = createBrowserHistory();
+import Login from './components/Login';
 
 export default function App() {
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <Router history={history}>{routes}</Router>
-      </PersistGate>
-    </Provider>
-  );
+    return <Login />;
 }
